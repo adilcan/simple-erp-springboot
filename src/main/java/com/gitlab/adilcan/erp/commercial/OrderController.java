5e373d76d6393bd717c2a7a8e4ab15d6f6bba415
tolgay.balci@hotmail.com <tolgay.balci@hotmailcom>
2018-01-20 12:27:47 +0300

    Add style to remaining views

--- src/main/java/com/gitlab/adilcan/erp/commercial/OrderController.java
+++ src/main/java/com/gitlab/adilcan/erp/commercial/OrderController.java
@@ -2,9 +2,12 @@ package com.gitlab.adilcan.erp.commercial;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.stereotype.Controller;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/orders")
@@ -26,7 +29,7 @@ public class OrderController {
     }
 
     @PostMapping("/new")
-    public String postNewOrder(@ModelAttribute Order order){
+    public String postNewOrder(@ModelAttribute @Valid Order order, BindingResult bindingResult){
         orderRepository.save(order);
         return "redirect:/orders";
     }
